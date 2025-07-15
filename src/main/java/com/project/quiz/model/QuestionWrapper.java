46c0dec829adbab00ace39e68e09ad00b9b28cb8
sravani5758 <lakshmisravanivishnubhotla@gmail.com>
2025-07-15 09:34:16 +0530

    removed cache

--- src/main/java/com/project/quiz/model/QuestionWrapper.java
+++ src/main/java/com/project/quiz/model/QuestionWrapper.java
@@ -1,9 +1,13 @@
 package com.project.quiz.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionWrapper {
 
     private Integer id;
@@ -13,14 +17,7 @@ public class QuestionWrapper {
     private String option3;
     private String option4;
 
-    public QuestionWrapper(Integer id, String questionTitle, String option1, String option2, String option3, String option4) {
-        this.id = id;
-        this.questionTitle = questionTitle;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
-    }
+
 
 
 }
--- src/main/java/com/project/quiz/service/QuestionService.java
+++ src/main/java/com/project/quiz/service/QuestionService.java
@@ -3,7 +3,7 @@ package com.project.quiz.service;
 import com.project.quiz.model.Question;
 import com.project.quiz.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,20 +16,18 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    @Cacheable
     public ResponseEntity<List<Question>> getAllQuestions() {
         try {
             return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
         } catch (Exception e) {
-            e.printStackTrace();
+           // e.printStackTrace();
             return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
         }
     }
-    @Cacheable
+
     public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
         return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
     }
-    @Cacheable
     public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
         return new ResponseEntity<>("succes",HttpStatus.CREATED);
